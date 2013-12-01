5cb2f85108c53e2f7407bd5ff5e7b46749d24813
Steven Sloan <stevenosloan@gmail.com>
2013-11-30 21:08:45 -0500

    add broken specs for when exceptions are thrown in catch::stdout blocks

--- spec/lib/catch_and_release/catch_spec.rb
+++ spec/lib/catch_and_release/catch_spec.rb
@@ -10,6 +10,34 @@ describe CatchAndRelease::Catch do
 
       expect( out ).to eq "hello world"
     end
+
+    context "with a non-default stdout" do
+      before :each do
+        @set_stdout = StringIO.new
+        $stdout = @set_stdout
+      end
+
+      it "returns $stdout to it's existing state after the call" do
+        described_class.stdout do
+          $stdout.print "hello world"
+        end
+
+        expect( $stdout ).to eq @set_stdout
+      end
+
+      context "when an exception is raised" do
+        it "returns $stdout to it's existing state after the call " do
+          begin
+            described_class.stdout do
+              raise "hello world"
+            end
+          rescue RuntimeError
+          end
+
+          expect( $stdout ).to eq @set_stdout
+        end
+      end
+    end
   end
 
   describe "::stderr" do
@@ -19,6 +47,34 @@ describe CatchAndRelease::Catch do
       end
 
       expect( out ).to eq "hello world"
+    end
+
+    context "with a non-default stderr" do
+      before :each do
+        @set_stderr = StringIO.new
+        $stderr = @set_stderr
+      end
+
+      it "returns $stderr to it's existing state after the call" do
+        described_class.stderr do
+          $stderr.print "hello world"
+        end
+
+        expect( $stderr ).to eq @set_stderr
+      end
+
+      context "when an exception is raised" do
+        it "returns $stderr to it's existing state after the call " do
+          begin
+            described_class.stderr do
+              raise "hello world"
+            end
+          rescue RuntimeError
+          end
+
+          expect( $stderr ).to eq @set_stderr
+        end
+      end
     end
   end
 
